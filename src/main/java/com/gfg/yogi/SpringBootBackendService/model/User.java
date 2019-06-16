410bca557ac30c9a55fdd8a8fe62967dec546f01
yougeshwer Dan Charan <danchara@adobe.com>
2019-06-16 06:20:28 +0530

    adds default constructor to User Model

--- src/main/java/com/gfg/yogi/SpringBootBackendService/model/User.java
+++ src/main/java/com/gfg/yogi/SpringBootBackendService/model/User.java
@@ -14,6 +14,9 @@ public class User {
     private String firstName;
     private String lastName;
 
+    public User() {
+    }
+
     public User(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
